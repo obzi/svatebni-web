9d63b5958d8c25af314173d722bda34c91c7b11d
Obzina Tomáš <Tomas.Obzina@synottech.com>
2025-06-08 12:00:04 +0200

    zmeny na webu

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import { useState, useEffect } from "react";
 const sections = [
 	{ id: "onas", label: "O nás" },
 	{ id: "misto", label: "Místo" },
+	{ id: "ubytovani", label: "Ubytování" },
 	{ id: "formular", label: "Formulář" },
 	{ id: "dresscode", label: "Dresscode" },	
 	{ id: "harmonogram", label: "Harmonogram" },	
-	{ id: "ubytovani", label: "Ubytování" },
 	{ id: "parkovani", label: "Parkování" },
 	{ id: "fotky", label: "Fotky" },
 	{ id: "dary", label: "Dary" },		
@@ -93,7 +93,7 @@ export default function App() {
 				{/* Menu */}
 				<nav className="sticky top-0 z-10 bg-white bg-opacity-90 shadow mb-6 mt-8 rounded-xl backdrop-blur">
 				<div className="flex items-center justify-between p-4">
-					<h1 className="text-xl font-serif text-[#5A3E36]">Svatba Verča & Tomáš</h1>
+					<h1 className="text-xl font-serif text-[#5A3E36]">Svatba Tom & Veru</h1>
 					<button
 					className="md:hidden"
 					onClick={() => setMenuOpen(!menuOpen)}
@@ -137,7 +137,7 @@ export default function App() {
 							className="w-full max-h-[400px] object-cover object-[center_42%] rounded-xl shadow-md"
 						/>
 						<p>
-						Jsme Verča a Tomáš – dva lidé, kteří se rozhodli spojit své cesty životem. Naše svatba bude oslavou nejen naší lásky, ale i všech krásných momentů, které jsme zažili a které nás ještě čekají. Děkujeme, že v tento den budete s námi. ❤️
+						Jsme Tom a Veru a za posledních pět let jsme nasbírali spoustu zážitků, razítek v pase a historek, které bychom možná neměli vyprávět nahlas. Spojuje nás chuť objevovat svět i sebe navzájem a smysl pro humor, bez kterého by to s námi nešlo. Společně jsme sjeli hory na prkně, rybníky na waku, oceán na surfu a půlku světa s batohem na zádech. Teď jsme se rozhodli naskočit do dalšího dobrodružství – manželství. A jak se známe, vezmeme to s nadhledem, humorem a nejspíš i helmou. Děkujeme, že jste u toho s námi.
 						</p>
 					</CardContent>
 				  </Card>
@@ -167,6 +167,46 @@ export default function App() {
 					  </div>
 					</CardContent>
 				  </Card>
+				</section>
+				
+				<section id="ubytovani" className="scroll-mt-24 p-6 bg-gray-200 max-w-md mx-auto rounded-xl space-y-4">
+				<h2 className="text-2xl font-serif text-[#5A3E36]">Ubytování</h2>
+				<Card className="bg-gray-200">
+				  <CardContent className="p-6 space-y-4 pt-6 bg-gray-200 rounded-xl">
+					<p>
+					  V hlavní budově v Dohnalově mlýně bude k dispozici omezený počet míst pro přespání, které máme určené pro nejbližší rodinu. Prosíme ty, které jsme neinfromovali o tom, že mají zajištěné místo na spaní, aby vyplnili ve formuláři níže, o jaké ubytování máte zájem.
+					Pokud chcete ušetřit a přespat zadarmo, je možné přespat na místě v Dohnalově mlýně, kde je velká louka se třemi glampingovými stany a dají se zde postavit i další. Případně je možné přijet i s karavanem nebo přespat v autě.
+					Pokud máte rádi pohodlí, doporučujeme co nejdříve rezervovat ubytování v penzionech v okolí, kam vám rádi zajistíme rozvoz, kdykoliv budete chtít.
+					Zde jsou ubytovací možnosti: 
+						<a href="https://www.motorestsamota.cz/ubytovani/"
+						  target="_blank"
+						  className="text-blue-600 underline"
+						>Motorest Samota</a>
+						<a href="https://www.e-chalupy.cz/ubytovani-zastrizly-probuzena-chalupa-k-pronajmu-o15926#mapa"
+						  target="_blank"
+						  className="text-blue-600 underline"
+						>Chata Zástřizly</a>
+						<a href="https://www.booking.com/hotel/cz/chaticka-hanicka-u-lesa.cs.html?label=cs-cz-booking-desktop-earHHgH3FZx9dDdzrjamRwS652828999198%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9211630%3Ali%3Adec%3Adm&aid=2311236&ucfs=1&checkin=2025-09-25&checkout=2025-09-26&dest_id=0&dest_type=latlong&group_adults=2&no_rooms=1&group_children=0&nflt=oos%3D1&srpvid=b3c98b4bc66f03e9&srepoch=1748980134&matching_block_id=1263414501_398988382_2_0_0&atlas_src=sr_iw_title"
+						  target="_blank"
+						  className="text-blue-600 underline"
+						>Chata Střílky</a>
+						<a href="https://www.booking.com/hotel/cz/penzion-vila-winter.cs.html?label=cs-cz-booking-desktop-earHHgH3FZx9dDdzrjamRwS652828999198%3Apl%3Ata%3Ap1%3Ap2%3Aac%3Aap%3Aneg%3Afi%3Atikwd-65526620%3Alp9211630%3Ali%3Adec%3Adm&aid=2311236&ucfs=1&checkin=2025-09-25&checkout=2025-09-26&dest_id=0&dest_type=latlong&group_adults=2&no_rooms=1&group_children=0&nflt=oos%3D1&srpvid=b3c98b4bc66f03e9&srepoch=1748980191&matching_block_id=751965801_412284979_2_0_0&atlas_src=sr_iw_title"
+						  target="_blank"
+						  className="text-blue-600 underline"
+						>Penzion Vila Winter</a>
+						<a href="https://pension-buchlovsky-dvur.penzion.cz/?gad_source=1&gad_campaignid=872974319&gbraid=0AAAAADMHvSJzuUgzIo4y-G2qkyZutXgAr&gclid=Cj0KCQjwuvrBBhDcARIsAKRrkjdo8_tOPiH2kUJnBQ8bk9HkbaHue2Y2ALPEU0PNYPyC61rR_lmEsSQaAnetEALw_wcB"
+						  target="_blank"
+						  className="text-blue-600 underline"
+						>Penzion Buchlovský dvůr</a>
+						Více na: e-chalupy.cz, booking.com, airbnb.cz
+					</p>
+					<img
+						src="./images//ubytko.jpg"
+						alt="Ubytování"
+						className="rounded-xl w-full object-cover max-h-[400px]"
+					/>
+				  </CardContent>
+				</Card>
 				</section>
 				
 				<section id="formular" className="scroll-mt-24 p-6 bg-gray-200 max-w-md mx-auto rounded-xl space-y-4">
@@ -234,24 +274,6 @@ export default function App() {
 				  </CardContent>
 				</Card>
 				</section>
-
-				<section id="ubytovani" className="scroll-mt-24 p-6 bg-gray-200 max-w-md mx-auto rounded-xl space-y-4">
-				<h2 className="text-2xl font-serif text-[#5A3E36]">Ubytování</h2>
-				<Card className="bg-gray-200">
-				  <CardContent className="p-6 space-y-4 pt-6 bg-gray-200 rounded-xl">
-					<p>
-					  V Dohnalově mlýně bude k dispozici omezený počet míst pro přespání. Pokud máte zájem o ubytování, uveďte to prosím ve formuláři níže. 
-					  <br/>
-					  Případně si můžete zajistit vlastní ubytování v okolí – rádi doporučíme.
-					</p>
-					<img
-						src="./images//ubytko.jpg"
-						alt="Ubytování"
-						className="rounded-xl w-full object-cover max-h-[400px]"
-					/>
-				  </CardContent>
-				</Card>
-				</section>
 				
 				<section id="parkovani" className="scroll-mt-24 p-6 bg-gray-200 max-w-md mx-auto rounded-xl space-y-4">
 				<h2 className="text-2xl font-serif text-[#5A3E36]">Parkování</h2>
@@ -307,7 +329,7 @@ export default function App() {
 							<p>Ozvěte se nám kdykoliv:</p>
 							<ul className="list-disc list-inside">
 							  <li><strong>Tomáš</strong>: <a href="tel:+420724963979" className="text-blue-600 underline">+420 724 963 979</a></li>
-							  <li><strong>Verča</strong>: <a href="tel:+420776026838" className="text-blue-600 underline">+420 776 026 838</a></li>
+							  <li><strong>Veru</strong>: <a href="tel:+420776026838" className="text-blue-600 underline">+420 776 026 838</a></li>
 							</ul>
 						</CardContent>
 					</Card>
